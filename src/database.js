09baf4c46cdd9528ad5451a68fc5d8f4a059935d
Igor Gonçalves <igordossantos38@gmail.com>
2023-07-31 15:14:46 -0300

    refactor: change from modules to es6 modules

--- src/database.js
+++ src/database.js
@@ -1,14 +1,16 @@
 // Esse arquivo é somente para conecção
 
-import mysql from "mysql";
+const mysql = require("mysql");
 
 /**
  * Simple connection but with no exported modules
  * @see https://www.w3schools.com/nodejs/nodejs_mysql.asp
  */
-export const connection = mysql.createConnection({
+const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
     database: "twomsenapp",
 });
+
+module.exports = connection
