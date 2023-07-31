dc0ee16b8342dd7464ac4bc7bd696515cd308fa9
Igor Gonçalves <igordossantos38@gmail.com>
2023-07-31 17:26:54 -0300

    fix: configure server to run css in public folder

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const userController =  require("./controllers/userController.js");
 const app = express()
 const port = 3000
 
+app.use(express.static('public'))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
