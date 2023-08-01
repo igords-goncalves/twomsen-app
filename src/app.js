a00178a5f378b3dc537f52d157f0ae95b6e73168
Igor Gonçalves <igordossantos38@gmail.com>
2023-08-01 09:28:14 -0300

    feat: create a body-parser instance and middlewares

--- src/app.js
+++ src/app.js
@@ -1,14 +1,20 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const path = require('path');
 const userController =  require("./controllers/userController.js");
 
 const app = express()
 const port = 3000
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.use(express.static('public'))
+app.use(express.static('js'))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 app.get('/', userController.getAllUsers)
+app.post('/', userController.createUser)
 
 app.listen(port, () => `Server running at port ${port}`)
